c70160486194b38c7437140b1d30d110180bf152
dishapadsala <dishapadshala20@gmail.com>
2025-02-28 15:22:36 -0500

    Added toString method for player information and setContinent() method to know if the continent is already assigned to a player in Player class

--- src/main/java/Model/Player.java
+++ src/main/java/Model/Player.java
@@ -1,19 +1,23 @@
 package Model;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Player {
     String d_playerName;
     Integer d_unallocatedArmies;
     List<Country> d_currentCountries;
-    List<Continent> d_currentContinents;
+    Set<Continent> d_currentContinents;
     List<Orders> d_orders;
 
     public Player(String p_playerName) {
         this.d_playerName = p_playerName;
         this.d_unallocatedArmies = 0;
         this.d_orders = new ArrayList<Orders>();
+        this.d_currentCountries = new ArrayList<Country>();
+        this.d_currentContinents = new HashSet<>();
     }
 
     public String getD_playerName() {
@@ -40,11 +44,11 @@ public class Player {
         this.d_currentCountries = p_currentCountries;
     }
 
-    public List<Continent> getD_currentContinents() {
+    public Set<Continent> getD_currentContinents() {
         return d_currentContinents;
     }
 
-    public void setD_currentContinents(List<Continent> p_currentContinents) {
+    public void setD_currentContinents(Set<Continent> p_currentContinents) {
         this.d_currentContinents = p_currentContinents;
     }
 
@@ -56,5 +60,14 @@ public class Player {
         this.d_orders = p_orders;
     }
 
+    public void setContinent(Continent p_continent) {
+        if(!d_currentContinents.add(p_continent)) {
+            System.out.println("Continent : "+p_continent.getD_continentName()+" already assigned to Player : "+d_playerName);
+        }
+    }
+
+    public String toString() {
+        return "Player [Player Name=" + d_playerName + ", Unallocated Armies=" + d_unallocatedArmies + ", Current Countries=" + d_currentCountries.size() + ", Current Continents=" + d_currentContinents.size() + ", Orders=" + d_orders.size() + "]";
+    }
 
 }
