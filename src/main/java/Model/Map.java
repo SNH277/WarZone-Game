8fd310fd2938d31f62816b7be1a239e6011b4919
Shrey Hingu <hingushrey2707@gmail.com>
2025-02-28 12:32:12 -0500

    created map class with getters, setters and tostring

--- src/main/java/Model/Map.java
+++ src/main/java/Model/Map.java
@@ -1,4 +1,42 @@
 package Model;
 
+import java.util.List;
+
 public class Map {
+    String d_mapName;
+    List<Country> d_mapCountries;
+    List<Continent> d_mapContinents;
+
+    public String getD_mapName() {
+        return d_mapName;
+    }
+
+    public void setD_mapName(String d_mapName) {
+        this.d_mapName = d_mapName;
+    }
+
+    public List<Continent> getD_mapContinents() {
+        return d_mapContinents;
+    }
+
+    public void setD_mapContinents(List<Continent> d_mapContinents) {
+        this.d_mapContinents = d_mapContinents;
+    }
+
+    public List<Country> getD_mapCountries() {
+        return d_mapCountries;
+    }
+
+    public void setD_mapCountries(List<Country> d_mapCountries) {
+        this.d_mapCountries = d_mapCountries;
+    }
+
+    @Override
+    public String toString() {
+        return "Map{" +
+                "d_mapName='" + d_mapName + '\'' +
+                ", d_mapCountries=" + d_mapCountries +
+                ", d_mapContinents=" + d_mapContinents +
+                '}';
+    }
 }
