0a3ea688e55600807acf033326583db359a6660c
Akhilesh-Kanbarkar <akhileshkanbarkaredu@gmail.com>
2025-02-28 14:42:22 -0500

    Created Orders.java class with variables, constructor, and getters and settters.

--- src/main/java/Model/Orders.java
+++ src/main/java/Model/Orders.java
@@ -1,4 +1,37 @@
 package Model;
 
 public class Orders {
+    String d_order;
+    String d_targetName;
+    Integer d_noOfArmiesToMove;
+
+    public Orders(String p_order, String p_targetName, Integer p_noOfArmiesToMove){
+        this.d_order = p_order;
+        this.d_targetName = p_targetName;
+        this.d_noOfArmiesToMove = p_noOfArmiesToMove;
+    }
+
+    public String getD_order() {
+        return d_order;
+    }
+
+    public String getD_targetName() {
+        return d_targetName;
+    }
+
+    public Integer getD_noOfArmiesToMove() {
+        return d_noOfArmiesToMove;
+    }
+
+    public void setD_order(String d_order) {
+        this.d_order = d_order;
+    }
+
+    public void setD_targetName(String d_targetName) {
+        this.d_targetName = d_targetName;
+    }
+
+    public void setD_noOfArmiesToMove(Integer d_noOfArmiesToMove) {
+        this.d_noOfArmiesToMove = d_noOfArmiesToMove;
+    }
 }
