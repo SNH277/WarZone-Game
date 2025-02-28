4baa7693fe10221ab6b053d253bbf9f88c854c5e
dishapadsala <dishapadshala20@gmail.com>
2025-02-28 15:36:46 -0500

    Added Variables and Constructor to MapView

--- src/main/java/View/MapView.java
+++ src/main/java/View/MapView.java
@@ -1,4 +1,18 @@
 package View;
 
+import Model.*;
+import java.util.List;
+
 public class MapView {
+    Map d_map;
+    List<Country> d_countries;
+    List<Continent> d_continents;
+    CurrentState d_currentState;
+
+    public MapView(CurrentState p_currentState) {
+        this.d_currentState = p_currentState;
+        this.d_map = p_currentState.getD_map();
+        this.d_countries = p_currentState.getD_map().getD_mapCountries();
+        this.d_continents = p_currentState.getD_map().getD_mapContinents();
+    }
 }
