572776ede05ff59ddf201f696239c4951bbc6f6b
dishapadsala <dishapadshala20@gmail.com>
2025-02-28 15:34:50 -0500

    Added Variables, Constructor, getters and setters to CurrentState

--- src/main/java/Model/CurrentState.java
+++ src/main/java/Model/CurrentState.java
@@ -1,4 +1,24 @@
 package Model;
 
+import java.util.List;
+
 public class CurrentState {
+    List<Player> d_players;
+    Map d_map;
+
+    public List<Player> getD_players() {
+        return d_players;
+    }
+
+    public void setD_players(List<Player> p_players) {
+        this.d_players = p_players;
+    }
+
+    public Map getD_map() {
+        return d_map;
+    }
+
+    public void setD_map(Map p_map) {
+        this.d_map = p_map;
+    }
 }
