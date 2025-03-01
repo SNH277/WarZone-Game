715804bd0d214ef3d8496e9daced0869274d7ce8
dishapadsala <dishapadshala20@gmail.com>
2025-03-01 18:15:37 -0500

    Implemented methods addPlayer() and removePlayer() in CurrentState

--- src/main/java/Model/CurrentState.java
+++ src/main/java/Model/CurrentState.java
@@ -36,9 +36,47 @@ public class CurrentState {
     }
 
     private void removePlayer(String p_command) {
+        if(p_command.split((" ")).length == 1){
+            String l_player = p_command.split(" ")[0];
+
+            if(d_players == null || d_players.isEmpty()) {
+                System.out.println("No players found.");
+                return;
+            }
+
+            Player l_player_found = getPlayerFromName(l_player);
+            if(l_player_found != null) {
+                d_players.remove(l_player_found);
+                System.out.println("Player " + l_player + " removed.");
+            } else {
+                System.out.println("Player " + l_player + " not found.");
+            }
+        }
+    }
+
+    private Player getPlayerFromName(String p_player) {
+        for(Player l_eachPlayer : d_players) {
+            if (l_eachPlayer.getD_playerName().equals(p_player)) {
+                return l_eachPlayer;
+            }
+        }
+        return null;
     }
 
     private void addPlayer(String p_command) {
+        if(p_command.split((" ")).length == 1){
+            String l_player = p_command.split(" ")[0];
+
+            if(d_players == null) {
+                d_players = new ArrayList<Player>();
+            } else if (d_players.stream().anyMatch(p -> p.getD_playerName().equals(l_player))) {
+                System.out.println("Player " + l_player + " already exists.");
+                return;
+            }
+
+            d_players.add(new Player(l_player));
+            System.out.println("Player " + l_player + " added.");
+        }
     }
 
 }
