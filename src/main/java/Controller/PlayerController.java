7a89047f5f0423a6a97dfb580e73a77de255f02a
dishapadsala <dishapadshala20@gmail.com>
2025-03-01 19:56:54 -0500

    Defined method for assigning country to player in PlayerController

--- src/main/java/Controller/PlayerController.java
+++ src/main/java/Controller/PlayerController.java
@@ -1,4 +1,45 @@
 package Controller;
 
+import Model.*;
+
+import java.util.List;
+
 public class PlayerController {
+    public void assignCountry(CurrentState p_currentState) {
+        if(p_currentState.getD_players() == null || p_currentState.getD_players().isEmpty()) {
+            System.out.println("No players found. Please add players using 'gameplayer -add playerName' command first.");
+            return;
+        }
+        if(p_currentState.getD_map() == null || p_currentState.getD_map().getD_mapCountries() == null) {
+            System.out.println("Map data is missing. Ensure a valid map is loaded before assigning countries");
+        }
+
+        List<Player> l_players = p_currentState.getD_players();
+        List<Country> l_countries = p_currentState.getD_map().getD_mapCountries();
+
+        int l_playerCount = l_players.size();
+        int l_countryCount = l_countries.size();
+        if (l_playerCount > l_countryCount) {
+            System.out.println("More players than available countries. Reduce player count or add more countries.");
+        }
+
+        int l_countriesPerPlayer = Math.floorDiv(l_countryCount, l_playerCount);
+
+        randomCountryDistribution(l_players, l_countries, l_countriesPerPlayer);
+
+        displayAssignedCountries(l_players);
+
+        updatePlayerContinentOwnership(l_players, p_currentState.getD_map().getD_mapContinents());
+    }
+
+    private void updatePlayerContinentOwnership(List<Player> p_Players, List<Continent> p_MapContinents) {
+    }
+
+    private void displayAssignedCountries(List<Player> p_Players) {
+    }
+
+    private void randomCountryDistribution(List<Player> p_Players, List<Country> p_Countries, int p_CountriesPerPlayer) {
+    }
+
+
 }
