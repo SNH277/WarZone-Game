f085071342b79b4cbcfb520744b668884d5fb5dc
Akhilesh-Kanbarkar <akhileshkanbarkaredu@gmail.com>
2025-03-07 12:52:28 -0500

    Minor changes

--- src/main/java/Model/Continent.java
+++ src/main/java/Model/Continent.java
@@ -82,7 +82,7 @@ public class Continent {
      * @param p_continentID the p continent id
      */
     public void setD_continentID(Integer p_continentID) {
-        this.d_continentID = d_continentID;
+        this.d_continentID = p_continentID;
     }
     /**
      * Sets d continent name.
@@ -90,7 +90,7 @@ public class Continent {
      * @param p_continentName the p continent name
      */
     public void setD_continentName(String p_continentName) {
-        this.d_continentName = d_continentName;
+        this.d_continentName = p_continentName;
     }
     /**
      * Sets d continent value.
@@ -98,7 +98,7 @@ public class Continent {
      * @param p_continentValue the p continent value
      */
     public void setD_continentValue(Integer p_continentValue) {
-        this.d_continentValue = d_continentValue;
+        this.d_continentValue = p_continentValue;
     }
     /**
      * Sets d countries.
@@ -106,7 +106,7 @@ public class Continent {
      * @param p_countries the p countries
      */
     public void setD_countries(List<Country> p_countries) {
-        this.d_countries = d_countries;
+        this.d_countries = p_countries;
     }
     /**
      * To string string.
