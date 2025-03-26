15db9b9bbe37ecdafc07e442293f96a0372b6cf1
Akhilesh-Kanbarkar <akhileshkanbarkaredu@gmail.com>
2025-03-26 12:31:44 -0400

    Added Deploy class with fields, constructor, getters, and toString method

---
+++ src/main/java/Model/Deploy.java
@@ -0,0 +1,53 @@
+package Model;
+
+
+import Model.Player;
+
+/**
+ * Represents a Deploy order in the game, where a player deploys armies to a country.
+ */
+public class Deploy{
+
+    private String d_targetCountryName;
+    private Integer d_noOfArmiesToMove;
+    private Player d_initiatingPlayer;
+
+    /**
+     * Instantiates a new Deploy order.
+     *
+     * @param p_initiatingPlayer the initiating player
+     * @param p_targetCountryName the target country name
+     * @param p_noOfArmiesToMove the number of armies to move
+     */
+    public Deploy(Player p_initiatingPlayer, String p_targetCountryName, Integer p_noOfArmiesToMove) {
+        this.d_initiatingPlayer = p_initiatingPlayer;
+        this.d_targetCountryName = p_targetCountryName;
+        this.d_noOfArmiesToMove = p_noOfArmiesToMove;
+    }
+
+    public String getTargetCountryName() {
+        return d_targetCountryName;
+    }
+
+    public Integer getNoOfArmiesToMove() {
+        return d_noOfArmiesToMove;
+    }
+
+    public Player getInitiatingPlayer() {
+        return d_initiatingPlayer;
+    }
+
+    /**
+     * Returns a string representation of the Deploy order.
+     *
+     * @return string representation
+     */
+    @Override
+    public String toString() {
+        return "Deploy{" +
+                "targetCountryName='" + d_targetCountryName + '\'' +
+                ", noOfArmiesToMove=" + d_noOfArmiesToMove +
+                ", initiatingPlayer=" + d_initiatingPlayer +
+                '}';
+    }
+}
