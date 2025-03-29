c77068719b110d416620fd764f71d85eab84ae01
Shrey Hingu <hingushrey2707@gmail.com>
2025-03-28 20:55:04 -0400

    test cases for order execution phase

--- src/test/java/Models/OrderExecutionPhaseTest.java
+++ src/test/java/Models/OrderExecutionPhaseTest.java
@@ -1,4 +1,94 @@
 package Models;
 
+import Controller.MainGameEngine;
+import Controller.MapController;
+import Model.*;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+/**
+ * The type Order execution phase test.
+ */
 public class OrderExecutionPhaseTest {
+    /**
+     * The D map.
+     */
+    Map d_map;
+    /**
+     * The D map controller.
+     */
+    MapController d_mapController;
+    /**
+     * The D current state.
+     */
+    CurrentState d_currentState;
+    /**
+     * The D main game engine.
+     */
+    MainGameEngine d_mainGameEngine;
+    /**
+     * The D player 1.
+     */
+    Player d_player1;
+    /**
+     * The D player 2.
+     */
+    Player d_player2;
+    /**
+     * The D order execution phase.
+     */
+    OrderExecutionPhase d_orderExecutionPhase;
+
+    /**
+     * Sets up.
+     */
+    @Before
+    public void setUp() {
+        d_currentState = new CurrentState();
+        d_mapController = new MapController();
+        d_mainGameEngine = new MainGameEngine();
+
+        d_map = d_mapController.loadMap(d_currentState, "test.map");
+        d_currentState.setD_map(d_map);
+
+        d_player1 = new Player("Player1");
+        d_player2 = new Player("Player2");
+        List<Player> l_players = List.of(d_player1, d_player2);
+        d_currentState.setD_players(l_players);
+
+        List<Country> l_player1Countries = new ArrayList<>();
+        List<Country> l_player2Countries = new ArrayList<>();
+        d_map.getCountryByName("USA").setD_armies(10);
+        d_map.getCountryByName("Canada").setD_armies(0);
+        l_player1Countries.add(d_map.getCountryByName("USA"));
+        l_player1Countries.add(d_map.getCountryByName("Canada"));
+        l_player1Countries.add(d_map.getCountryByName("France"));
+        l_player1Countries.add(d_map.getCountryByName("UK"));
+        l_player1Countries.add(d_map.getCountryByName("Germany"));
+        l_player1Countries.add(d_map.getCountryByName("Argentina"));
+        l_player1Countries.add(d_map.getCountryByName("Chile"));
+        l_player2Countries.add(d_map.getCountryByName("Brazil"));
+        d_player1.setD_currentCountries(l_player1Countries);
+        d_player2.setD_currentCountries(l_player2Countries);
+
+        d_orderExecutionPhase = new OrderExecutionPhase(d_currentState, d_mainGameEngine);
+    }
+
+    /**
+     * Check end of game.
+     */
+    @Test
+    public void checkEndOfGame() {
+        assertFalse(d_orderExecutionPhase.checkEndOfGame(d_currentState));
+        Orders l_advanceOrder = new Advance("USA", "Brazil", 9, d_player1);
+        assertEquals(0,d_player1.getD_cardOwnedByPlayer().size());
+        l_advanceOrder.execute(d_currentState);
+        assertEquals(1,d_player1.getD_cardOwnedByPlayer().size());
+        assertTrue(d_orderExecutionPhase.checkEndOfGame(d_currentState));
+    }
 }
