41a4886b939b5774ed066ecfeb3ee2a8d90cd93f
Akhilesh-Kanbarkar <akhileshkanbarkaredu@gmail.com>
2025-04-08 12:47:19 -0400

    Created GameService class with constructor and documentation

--- src/main/java/Services/GameService.java
+++ src/main/java/Services/GameService.java
@@ -1,5 +1,20 @@
 package Services;
 
+import Model.*;
+
+import java.io.*;
+
+/**
+ * GameService handles saving and loading the game state (Phase object) using serialization.
+ *
+ * Author: Akhilesh Kanbarkar
+ */
 public class GameService {
+
+    /**
+     * Default constructor.
+     */
+    public GameService() {
+    }
 
 }
