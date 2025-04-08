4e76714a82f5fd0e0f7c48b6f2611dbbb6d14c1f
Akhilesh-Kanbarkar <akhileshkanbarkaredu@gmail.com>
2025-04-08 12:26:30 -0400

    Created ConquestMapFileReader class with constructor and base structure

--- src/main/java/Services/ConquestMapFileReader.java
+++ src/main/java/Services/ConquestMapFileReader.java
@@ -1,5 +1,23 @@
 package Services;
 
-public class ConquestMapFileReader {
+import Model.*;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Reads a map file in Conquest format and parses it into the application's internal data structures.
+ * Supports parsing of continent, country, and neighbor data.
+ *
+ * Author: Akhilesh Kanbarkar
+ */
+public class ConquestMapFileReader implements Serializable {
+
+    /**
+     * Default constructor.
+     */
+    public ConquestMapFileReader() {
+    }
 
 }
