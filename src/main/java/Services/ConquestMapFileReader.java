77cd8e40fbf589e5265bac68f3f15438048a5cd1
Akhilesh-Kanbarkar <akhileshkanbarkaredu@gmail.com>
2025-04-08 12:29:48 -0400

    Added readConquestFile method to parse conquest-format file and update state

--- src/main/java/Services/ConquestMapFileReader.java
+++ src/main/java/Services/ConquestMapFileReader.java
@@ -19,5 +19,26 @@ public class ConquestMapFileReader implements Serializable {
      */
     public ConquestMapFileReader() {
     }
+    /**
+     * Parses a conquest-format map file and populates the map and current state objects.
+     *
+     * @param p_currentState the current game state to update
+     * @param p_map the map object to populate
+     * @param p_fileLines the lines read from the map file
+     * @param p_fileName the name of the map file
+     */
+    public void readConquestFile(CurrentState p_currentState, Map p_map, List<String> p_fileLines, String p_fileName) {
+        List<String> l_continentData = getMetaData(p_fileLines, "continent");
+        List<Continent> l_continentList = parseContinentMetaData(l_continentData);
 
+        List<String> l_countryData = getMetaData(p_fileLines, "country");
+        List<Country> l_countryList = parseCountryMetaData(l_countryData, l_continentList);
+        List<Country> l_updatedCountryList = parseNeighboursMetaData(l_countryList, l_countryData);
+
+        l_continentList = linkCountryToContinent(l_continentList, l_updatedCountryList);
+        p_map.setD_mapContinents(l_continentList);
+        p_map.setD_mapCountries(l_updatedCountryList);
+        p_map.setD_mapName(p_fileName);
+        p_currentState.setD_map(p_map);
+    }
 }
