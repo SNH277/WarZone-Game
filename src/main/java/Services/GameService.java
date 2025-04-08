e7faf69a508d6dbf7b3397a8e0916ca7d72265c3
Akhilesh-Kanbarkar <akhileshkanbarkaredu@gmail.com>
2025-04-08 12:55:28 -0400

    Added saveGame method to serialize and save current game phase

--- src/main/java/Services/GameService.java
+++ src/main/java/Services/GameService.java
@@ -16,5 +16,22 @@ public class GameService {
      */
     public GameService() {
     }
+    /**
+     * Serializes and saves the current game phase to a file.
+     *
+     * @param p_currentPhase The current phase object to save.
+     * @param p_fileName The name of the file to which the game will be saved.
+     */
+    public static void saveGame(Phase p_currentPhase, String p_fileName){
+        try {
+            FileOutputStream l_gameSaveFile = new FileOutputStream("src/main/maps/" + p_fileName);
+            ObjectOutputStream l_gameSaveFileObjectStream = new ObjectOutputStream(l_gameSaveFile);
+            l_gameSaveFileObjectStream.writeObject(p_currentPhase);
+            l_gameSaveFileObjectStream.flush();
+            l_gameSaveFileObjectStream.close();
+        } catch (Exception l_e) {
+            l_e.printStackTrace();
+        }
+    }
 
 }
