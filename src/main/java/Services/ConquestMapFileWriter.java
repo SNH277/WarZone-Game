6e0b351eae1123a7f85353bbe21b59eb91d2ff43
Akhilesh-Kanbarkar <akhileshkanbarkaredu@gmail.com>
2025-04-08 12:20:20 -0400

    Added parseMapToFile method

--- src/main/java/Services/ConquestMapFileWriter.java
+++ src/main/java/Services/ConquestMapFileWriter.java
@@ -18,5 +18,21 @@ public class ConquestMapFileWriter implements Serializable {
      */
     public ConquestMapFileWriter() {
     }
+    /**
+     * Parses the current game state and writes map data to a file in the specified map format.
+     *
+     * @param p_currentState The current game state containing map data.
+     * @param p_writer The FileWriter object used to write to the file.
+     * @param p_mapFormat The format of the map (currently unused, reserved for future use).
+     * @throws IOException if an I/O error occurs.
+     */
+    public void parseMapToFile(CurrentState p_currentState, FileWriter p_writer, String p_mapFormat) throws IOException {
+        if (p_currentState.getD_map().getD_mapContinents() != null && !p_currentState.getD_map().getD_mapContinents().isEmpty()) {
+            writeContinentMetaData(p_currentState, p_writer);
+        }
+        if (p_currentState.getD_map().getD_mapCountries() != null && !p_currentState.getD_map().getD_mapCountries().isEmpty()) {
+            writeCountryAndBorderMetaData(p_currentState, p_writer);
+        }
+    }
 
 }
