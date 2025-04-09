650ba3f88df90d707ad6f5d661ca59fca6780a47
dishapadsala <dishapadshala20@gmail.com>
2025-04-08 20:38:27 -0400

    Added doubleArmyOnEnemyNeighbourCountries and conquerNeighboringEnemies methods in CheaterPlayer.java

--- src/main/java/Model/CheaterPlayer.java
+++ src/main/java/Model/CheaterPlayer.java
@@ -44,10 +44,57 @@ public class CheaterPlayer extends PlayerBehaviourStrategy{
         return null;
     }
 
-    private void doubleArmyOnEnemyNeighbourCountries(Player pPlayer, CurrentState pCurrentState) {
+    private Country getRandomCountry(List<Country> dCurrentCountries) {
     }
 
-    private void conquerNeighboringEnemies(Player pPlayer, CurrentState pCurrentState) {
+    private void doubleArmyOnEnemyNeighbourCountries(Player p_player, CurrentState p_currentState) {
+        List<Country> l_countriesOwned = p_player.getD_currentCountries();
+
+        for(Country l_eachCountry : l_countriesOwned){
+            ArrayList<Integer> l_countryEnemies = getEnemies(p_player, l_eachCountry);
+
+            if(l_countryEnemies.size() == 0) {
+                continue;
+            }
+            Integer l_armies = l_eachCountry.getD_armies();
+
+            if(l_armies == 0) {
+                continue;
+            }
+            l_eachCountry.setD_armies(l_armies * 2);
+            String l_logMessage = "Cheater Player: " + p_player.getD_playerName() +
+                    " doubled the armies on " + l_eachCountry.getD_countryName();
+
+            p_currentState.updateLog(l_logMessage,"effect");
+        }
+    }
+
+    private ArrayList<Integer> getEnemies(Player pPlayer, Country lEachCountry) {
+    }
+
+    private void conquerNeighboringEnemies(Player p_player, CurrentState p_currentState) {
+        List<Country> l_countriesOwned = p_player.getD_currentCountries();
+
+        for(Country l_eachCountry : l_countriesOwned){
+            ArrayList<Integer> l_countryEnemies = getEnemies(p_player, l_eachCountry);
+
+            for(Integer l_enemyId: l_countryEnemies) {
+                Map l_map = p_currentState.getD_map();
+                Player l_enemyCountryOwner = getCountryOwner(p_currentState, l_enemyId);
+                Country l_enemyCountry =l_map.getCountryById(l_enemyId);
+                conquerTargetCountry(p_currentState, l_enemyCountryOwner, p_player, l_enemyCountry);
+                String l_logMessage = "Cheater Player: " + p_player.getD_playerName() +
+                        " conquered " + l_enemyCountry.getD_countryName() +
+                        " from " + l_enemyCountryOwner.getD_playerName();
+                p_currentState.updateLog(l_logMessage, "effect");
+            }
+        }
+    }
+
+    private void conquerTargetCountry(CurrentState pCurrentState, Player lEnemyCountryOwner, Player pPlayer, Country lEnemyCountry) {
+    }
+
+    private Player getCountryOwner(CurrentState pCurrentState, Integer lEnemyId) {
     }
 
 }
